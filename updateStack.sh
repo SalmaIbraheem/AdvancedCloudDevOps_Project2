9c561e8152274ab3be7e1bd1ab77c1e81669dbc7
Salma Ibrahim <34141325+SalmaIbraheem@users.noreply.github.com>
2022-11-05 22:00:30 +0200

    add cloudfromation stacks yml files

---
+++ updateStack.sh
@@ -0,0 +1,2 @@
+/usr/local/bin/aws cloudformation update-stack --stack-name $1 --template-body file://$2  --parameters file://$3 --capabilities "CAPABILITY_IAM" "CAPABILITY_NAMED_IAM" --region=us-east-1 --disable-rollback
+
